dc50921bcbf03f95af6b00ddbc9539508156493f
evanaubrey <evan@bitspacestudios.com>
2017-10-18 10:12:55 -0700

    adding firebase auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,27 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
+
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
+
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = 'LoginPage';
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyC2gGVlBGrBxTS4IxauvXq-HsFLwcnNHtM",
+      authDomain: "llama-lounge.firebaseapp.com",
+      databaseURL: "https://llama-lounge.firebaseio.com",
+      projectId: "llama-lounge",
+      storageBucket: "llama-lounge.appspot.com",
+      messagingSenderId: "18272820898"
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -19,4 +31,12 @@ export class MyApp {
     });
   }
 }
-
+const unsubscribe = firebase.auth().onAuthStateChanged( user => {
+  if (!user) {
+    this.rootPage = 'LoginPage';
+    unsubscribe();
+  } else {
+    this.rootPage = HomePage;
+    unsubscribe();
+  }
+});
